4628213962e26cec9e519936c6ccdb06edee8447
kranthiB <kranthi.b76@gmail.com>
2017-07-25 13:39:38 +0530

    Removed Un-necessary classes

--- registry.py
+++ registry.py
@@ -2,8 +2,6 @@ from nlu.utils.spacy_utils import SpacyNLP
 from nlu.tokenizers.spacy_tokenizer import SpacyTokenizer
 from nlu.featurizers.spacy_featurizer import SpacyFeaturizer
 from nlu.featurizers.regex_featurizer import RegexFeaturizer
-from nlu.extractors.crf_entity_extractor import CRFEntityExtractor
-from nlu.extractors.entity_synonyms import EntitySynonymMapper
 from nlu.classifiers.sklearn_intent_classifier import SklearnIntentClassifier
 
 registered_pipeline_templates = {
@@ -12,51 +10,7 @@ registered_pipeline_templates = {
         "tokenizer_spacy",
         "intent_featurizer_spacy",
         "intent_entity_featurizer_regex",
-        "ner_crf",
-        "ner_synonyms",
-        "intent_classifier_sklearn",
-    ],
-    "mitie": [
-        "nlp_mitie",
-        "tokenizer_mitie",
-        "ner_mitie",
-        "ner_synonyms",
-        "intent_entity_featurizer_regex",
-        "intent_classifier_mitie",
-    ],
-    "mitie_sklearn": [
-        "nlp_mitie",
-        "tokenizer_mitie",
-        "ner_mitie",
-        "ner_synonyms",
-        "intent_entity_featurizer_regex",
-        "intent_featurizer_mitie",
-        "intent_classifier_sklearn",
-    ],
-    "keyword": [
-        "intent_classifier_keyword",
-    ],
-    # this template really is just for testing
-    # every component should be in here so train-persist-load-use cycle can be tested
-    # they still need to be in a useful order - hence we can not simply generate this automatically
-    "all_components": [
-        "nlp_spacy",
-        "nlp_mitie",
-        "tokenizer_whitespace",
-        "tokenizer_mitie",
-        "tokenizer_spacy",
-        "intent_featurizer_mitie",
-        "intent_featurizer_spacy",
-        "intent_featurizer_ngrams",
-        "intent_entity_featurizer_regex",
-        "ner_mitie",
-        "ner_crf",
-        "ner_spacy",
-        "ner_duckling",
-        "ner_synonyms",
-        "intent_classifier_keyword",
-        "intent_classifier_sklearn",
-        "intent_classifier_mitie",
+        "intent_classifier_sklearn"
     ]
 }
 
@@ -65,8 +19,6 @@ component_classes = [
 	SpacyTokenizer,
 	SpacyFeaturizer,
 	RegexFeaturizer,
-	CRFEntityExtractor,
-	EntitySynonymMapper,
 	SklearnIntentClassifier
 ]
 
